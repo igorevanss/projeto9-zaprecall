591f6275991d5bc8af90ca4654525085eeb55e35
igorevanss <igorevans.pro@gmail.com>
2022-10-03 19:30:43 -0300

    projeto conclúído

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,22 +6,33 @@ import almost from '../assets/img/icone_quase.png'
 import correct from '../assets/img/icone_certo.png'
 import styled from 'styled-components'
 
-function TurnedQuestion({ question, answer }) {
+function TurnedQuestion({ question, answer, clickedBtn, index }) {
   const [showAnswer, setShowAnswer] = useState(question)
 
   return (
-    <PerguntaAberta
-      data-identifier="flashcard-index-item flashcard-answer"
-      className="pergunta-aberta"
-    >
-      <p>{showAnswer}</p>
-      <img
-        data-identifier="flashcard-turn-btn"
-        onClick={() => setShowAnswer(answer)}
-        src={setaVirar}
-        alt="turn"
-      />
-    </PerguntaAberta>
+    <>
+      {clickedBtn === 'Não lembrei' ||
+      clickedBtn === 'Quase não lembrei' ||
+      clickedBtn === 'Zap!' ? (
+        <PerguntaRespondida clickedBtn={clickedBtn} data-identifier="flashcard">
+          <p>{`Pergunta ${index + 1}`}</p>
+          <img src={clickedBtn === 'Não lembrei' ? error : clickedBtn === 'Quase não lembrei' ? almost : clickedBtn === 'Zap!' ? correct : play} alt="Play" />
+        </PerguntaRespondida>
+      ) : (
+        <PerguntaAberta
+          data-identifier="flashcard-index-item flashcard-answer"
+          className="pergunta-aberta"
+        >
+          <p>{showAnswer}</p>
+          <img
+            data-identifier="flashcard-turn-btn"
+            onClick={() => setShowAnswer(answer)}
+            src={setaVirar}
+            alt="turn"
+          />
+        </PerguntaAberta>
+      )}
+    </>
   )
 }
 
@@ -35,7 +46,7 @@ export default function Question({
 }) {
   return (
     <>
-      {!tap || clickedBtn === 'Não lembrei' || clickedBtn === 'Quase não lembrei' || clickedBtn === 'Zap!' ? (
+      {!tap ? (
         <PerguntaFechada clickedBtn={clickedBtn} data-identifier="flashcard">
           <p>{`Pergunta ${index + 1}`}</p>
           <img
@@ -46,15 +57,21 @@ export default function Question({
           />
         </PerguntaFechada>
       ) : (
-        <TurnedQuestion question={question} answer={answer} />)}
+        <TurnedQuestion
+          question={question}
+          answer={answer}
+          clickedBtn={clickedBtn}
+          index={index}
+        />
+      )}
     </>
   )
 }
 
-const PerguntaFechada = styled.div`
+const PerguntaRespondida = styled.div`
   width: 300px;
   height: 35px;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   margin: 12px;
   padding: 15px;
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
@@ -64,21 +81,55 @@ const PerguntaFechada = styled.div`
   justify-content: space-between;
 
   p {
-  font-family: 'Recursive', sans-serif;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 16px;
-  line-height: 19px;
-  color: #333333;
-  
-}
+    font-family: 'Recursive', sans-serif;
+    text-decoration: line-through;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    color: ${props => {
+      const color = props.clickedBtn
+
+      if (color === 'Não lembrei') {
+        return '#FF3030'
+      } else if (color === 'Quase não lembrei') {
+        return '#FF922E'
+      } else if(color === 'Zap!') {
+        return '#2FBE34'
+      } else {
+        return "#333333"
+      }
+    }};
+  }
+`
+
+const PerguntaFechada = styled.div`
+  width: 300px;
+  height: 35px;
+  background-color: #ffffff;
+  margin: 12px;
+  padding: 15px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  p {
+    font-family: 'Recursive', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    color: #333333;
+  }
 `
 const PerguntaAberta = styled.div`
   width: 300px;
   margin: 12px;
   padding: 15px;
   min-height: 100px;
-  background: #FFFFD5;
+  background: #ffffd5;
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
   border-radius: 5px;
   font-family: 'Recursive', sans-serif;
@@ -92,9 +143,9 @@ const PerguntaAberta = styled.div`
   flex-direction: column;
   justify-content: space-between;
 
-  img{
-  position: absolute;
-  bottom: 10px;
-  right: 10px;
-}
+  img {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
 `
